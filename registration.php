de50bdf33cfbbb8ad16d71a9e9beb6c6edae7a50
Didier Berlioz <didier.berlioz@hagergroup-external.com>
2024-01-27 17:17:56 +0100

    Addeos_ConfigHub first commit

---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @license http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @author didier <didier@addeos.com>
+ * @copyright Copyright (c) 2024 Addeos (http://www.addeos.com)
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Addeos_ConfigHub', __DIR__);
